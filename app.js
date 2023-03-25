4abad67eb8a2022beee841be1fc0a0b86406431e
Mareline <mrami204@fiu.edu>
2023-03-25 17:48:12 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+//Helpers
+function randomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+function getKeyString(x, y) {
+    return `${x}x${y}`;
+}
+
+(function () {
+    firebase.auth().signInAnonymlously()
+})();
